261536e5aae12f70509db777e37b8597847669be
Farhan Azam <Farhan.Azam@deutics.com>
2023-07-20 12:50:22 +0500

    added api endpoints file

---
+++ lib/app/constants/api_endpoints.dart
@@ -0,0 +1,6 @@
+class ApiConstants{
+  static String baseUrl = "http://34.65.200.85/api/v1";
+  static String apiUrl = "${baseUrl}/api/v1/";
+  static String authUrl = "${apiUrl}auth/";
+  static String googleLogin = "${authUrl}google_login_mobile";
+}
