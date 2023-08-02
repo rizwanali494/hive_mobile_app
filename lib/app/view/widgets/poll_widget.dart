2e2af68b5bb2953ab7c0d18bb8c0adaa748c36c5
Farhan Azam <farhan19300@gmail.com>
2023-08-02 15:07:09 +0500

    refracted poll widget

--- lib/app/view/widgets/poll_widget.dart
+++ lib/app/view/widgets/poll_widget.dart
@@ -22,64 +22,88 @@ class PollWidget extends StatelessWidget {
       width: double.infinity,
       child: Padding(
         padding: EdgeInsets.only(bottom: 16.h),
-        child: IntrinsicWidth(
-          child: Stack(
-            alignment: AlignmentDirectional.center,
-            children: [
-              LinearPercentIndicator(
-                lineHeight: 35.h,
-                percent: controller.pollPercentage,
-                backgroundColor: styles.smokeWhite,
-                progressColor: selected
-                    ? styles.lavender
-                    : styles.platinum,
-                barRadius: const Radius.circular(36),
-                padding: EdgeInsets.zero,
-              ),
-              Center(
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 12.w),
-                  child: Row(
-                    children: [
-                      Radio<bool>(
-                        value: true,
-                        fillColor: MaterialStateProperty.resolveWith ((Set  states) {
-                          if (states.contains(MaterialState.selected)) {
-                            return styles.deepSkyBlue;
-                          }
-                          return Colors.white;
-                        }),
-                        activeColor: styles.deepSkyBlue,
-                        visualDensity: const VisualDensity(
-                            horizontal: VisualDensity.minimumDensity,
-                            vertical: VisualDensity.minimumDensity),
-                        materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                        groupValue: false,
-                        onChanged: (val) {},
-                      ),
-                      10.horizontalSpace,
-                      Expanded(
-                        child: Text(
-                          controller.labelText,
-                          style: styles.inter12w400,
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 1,
-                        ),
-                      ),
-                      10.horizontalSpace,
-                      Align(
-                        alignment: Alignment.centerRight,
-                        child: Text(
-                          "${controller.pollPercentageString}%",
-                          // AppStrings.percent67,
-                          style: styles.inter12w600,
-                        ),
-                      ),
-                    ],
+        child: IntrinsicHeight(
+          child: IntrinsicWidth(
+            child: Stack(
+              alignment: AlignmentDirectional.center,
+              children: [
+                // LinearPercentIndicator(
+                //   // lineHeight: 35.h,
+                //   percent: controller.pollPercentage,
+                //   backgroundColor: styles.smokeWhite,
+                //   progressColor: selected
+                //       ? styles.lavender
+                //       : styles.platinum,
+                //   barRadius: const Radius.circular(36),
+                //   padding: EdgeInsets.zero,
+                // ),
+                Container(
+                  // height: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(36.r),
+                    color: styles.smokeWhite,
                   ),
                 ),
-              ),
-            ],
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: FractionallySizedBox(
+                    widthFactor: controller.pollPercentage,
+                    alignment: Alignment.topLeft,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(36.r),
+                        color: selected
+                              ? styles.lavender
+                              : styles.platinum,
+                      ),
+                    ),
+                  ),
+                ),
+                Center(
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 12.w),
+                    child: Row(
+                      children: [
+                        Radio<bool>(
+                          value: true,
+                          fillColor: MaterialStateProperty.resolveWith ((Set  states) {
+                            if (states.contains(MaterialState.selected)) {
+                              return styles.deepSkyBlue;
+                            }
+                            return Colors.white;
+                          }),
+                          activeColor: styles.deepSkyBlue,
+                          visualDensity: const VisualDensity(
+                              horizontal: VisualDensity.minimumDensity,
+                              vertical: VisualDensity.minimumDensity),
+                          materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                          groupValue: false,
+                          onChanged: (val) {},
+                        ),
+                        10.horizontalSpace,
+                        Expanded(
+                          child: Text(
+                            controller.labelText,
+                            style: styles.inter12w400,
+                            // overflow: TextOverflow.ellipsis,
+                            // maxLines: 1,
+                          ),
+                        ),
+                        10.horizontalSpace,
+                        Align(
+                          alignment: Alignment.centerRight,
+                          child: Text(
+                            "${controller.pollPercentageString}%",
+                            // AppStrings.percent67,
+                            style: styles.inter12w600,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
