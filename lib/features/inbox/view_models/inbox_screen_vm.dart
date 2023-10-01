2a47d4bdb055ab0264c3c34f96a6c6df65109cc1
Farhan Azam <Farhan19300@gmail.com>
2023-10-01 20:19:34 +0500

    developing inbox search

--- lib/features/inbox/view_models/inbox_screen_vm.dart
+++ lib/features/inbox/view_models/inbox_screen_vm.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:get_it/get_it.dart';
 import 'package:hive_mobile/app/models/data/inbox_model.dart';
+import 'package:hive_mobile/app/models/ui_state_model.dart';
 import 'package:hive_mobile/app/services/api_services/api_services.dart';
 import 'package:hive_mobile/app/view_models/base_api_vm.dart';
 import 'package:hive_mobile/features/inbox/repositories/inbox_repository.dart';
@@ -31,5 +33,77 @@ class InboxScreenVM extends BaseApiVM<InboxModel> {
 
   List<InboxModel> get newConversation {
     return items.where((element) => element.content == null).toList();
+  }
+
+  final searchCtrl = TextEditingController();
+
+  bool get isSearching {
+    return searchCtrl.text.isNotEmpty;
+  }
+
+  Future<void> searchInbox() async {
+    final text = searchCtrl.text.trim();
+    if (text.isEmpty) {
+      return;
+    }
+  }
+}
+
+class InboxSearchVM extends BaseApiVM<InboxModel> {
+  InboxScreenVM() {
+    super.uiState = UiState.loaded();
+  }
+
+  @override
+  Future<void> getInitialItems() async {
+    final text = controller.text.trim();
+    if (text.isEmpty) {
+      return;
+    }
+    final request = () async {
+      var list = await fetchInitialItems();
+      if (list.length < limit) {
+        paginationController.isLastPage = true;
+      } else {
+        paginationController.setOffset(list.length);
+      }
+      items.addAll(list);
+      return;
+    };
+    await performRequest(request: request);
+    uiState = UiState.loaded();
+    notifyListeners();
+    // super.getInitialItems();
+  }
+
+  @override
+  Future<List<InboxModel>> fetchInitialItems() {
+    // TODO: implement fetchInitialItems
+    throw UnimplementedError();
+  }
+
+
+
+  @override
+  Future<List<InboxModel>> fetchNextItems() {
+    // TODO: implement fetchNextItems
+    throw UnimplementedError();
+  }
+
+  @override
+  void setRepoInstance() {
+
+  }
+
+  @override
+  Future<List<InboxModel>> fetchLocalList() async {
+    return const <InboxModel>[];
+  }
+
+  final controller = TextEditingController();
+
+  void search() {
+    getInitialItems();
+
   }
 }
