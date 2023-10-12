fbe234ffcef998d638912e41ee694e25eb6d7c73
Farhan Azam <Farhan.Azam@deutics.com>
2023-10-12 12:23:11 +0500

    University application vm refactor

---
+++ lib/features/university_application/view_models/accepted_application_vm.dart
@@ -0,0 +1,28 @@
+import 'package:hive_mobile/app/models/data/university_application/university_application_model.dart';
+import 'package:hive_mobile/features/university_application/view_models/university_application_screen_vm.dart';
+import 'package:isar/isar.dart';
+
+class AcceptedApplicationVM extends BaseUniversityApplicationScreenVM {
+  @override
+  Future<List<UniversityApplicationModel>> fetchInitialItems(
+      {required int limit}) async {
+    final list = await universityApplicationRepository.getAcceptedApplications(
+        limit: limit);
+    return list;
+  }
+
+  @override
+  Future<List<UniversityApplicationModel>> fetchLocalList() async {
+    final list =
+        await isarService.query().filter().stateEqualTo("APPROVED").findAll();
+    return list;
+  }
+
+  @override
+  Future<List<UniversityApplicationModel>> fetchNextItems(
+      {required int limit, required int offset}) async {
+    final list = await universityApplicationRepository.getAcceptedApplications(
+        limit: limit, offSet: offset);
+    return list;
+  }
+}
--- lib/features/university_application/view_models/university_application_screen_vm.dart
+++ lib/features/university_application/view_models/university_application_screen_vm.dart
@@ -1,11 +1,12 @@
 import 'dart:developer';
-
+import 'package:collection/collection.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get_it/get_it.dart';
 import 'package:hive_mobile/app/exceptions/http_status_code_exception.dart';
 import 'package:hive_mobile/app/models/data/university_application/university_application_model.dart';
 import 'package:hive_mobile/app/resources/app_strings.dart';
 import 'package:hive_mobile/app/services/api_services/api_services.dart';
+import 'package:hive_mobile/app/services/local_services/isar_service.dart';
 
 import 'package:hive_mobile/features/university_application/repositories/university_application_repo.dart';
 import 'package:isar/isar.dart';
@@ -295,33 +296,24 @@ class UniversityApplicationScreenVM extends ChangeNotifier {
   }
 }
 
-class BaseUniversityApplicationScreenVM extends ChangeNotifier {
+abstract class BaseUniversityApplicationScreenVM extends ChangeNotifier {
   bool _isAcceptedLoading = true;
-  bool _isPreviousLoading = true;
   bool _isGettingMoreAccepted = false;
-  bool _isGettingMorePrevious = false;
-  bool _hasAllAccepted = false;
-  bool _hasAllPrevious = false;
-  int _acceptedAppOffset = 0;
-  int _previousAppOffset = 0;
+  bool _hasAll = false;
+  int offset = 0;
   final _limit = 10;
 
   bool get isGettingMoreAccepted => _isGettingMoreAccepted;
 
   bool get isAcceptedLoading => _isAcceptedLoading;
 
-  bool get isPreviousLoading => _isPreviousLoading;
-
-  bool get hasAllAccepted => _hasAllAccepted;
-
-  bool get hasAllPrevious => _hasAllPrevious;
+  bool get hasAllAccepted => _hasAll;
 
   late UniversityApplicationRepository universityApplicationRepository;
 
   ApiService apiService = GetIt.instance.get<ApiService>();
 
-  List<UniversityApplicationModel> acceptedApplications = [];
-  List<UniversityApplicationModel> previousApplications = [];
+  List<UniversityApplicationModel> applications = [];
 
   UniversityApplicationScreenVM() {
     universityApplicationRepository =
@@ -330,41 +322,32 @@ class BaseUniversityApplicationScreenVM extends ChangeNotifier {
   }
 
   Future<void> getApplications() async {
-    await setIsarInstances();
-    await getLocalSaveApplication();
     await getAcceptedApplications();
-    await getPreviousApplications();
   }
 
-  Future<void> getLocalSaveApplication() async {
-    try {
-      var list = await getLocalApplications(acceptedAppIsar);
-      acceptedApplications.addAll(list);
-      acceptedApplications = acceptedApplications.toSet().toList();
-      var previousList = await getLocalApplications(previousAppIsar);
-      previousApplications.addAll(previousList);
-      previousApplications = previousApplications.toSet().toList();
-    } catch (e) {
-      log("isar error : ${e.toString()}");
+  Future<List<UniversityApplicationModel>> fetchInitialItems(
+      {required int limit});
 
-      // TODO
-    }
-  }
+  Future<List<UniversityApplicationModel>> fetchNextItems(
+      {required int limit, required int offset});
+
+  Future<List<UniversityApplicationModel>> fetchLocalList();
 
   Future<void> getAcceptedApplications() async {
     final request = () async {
-      var list = await universityApplicationRepository.getAcceptedApplications(
-          limit: _limit);
+      var list = await fetchInitialItems(limit: _limit);
       if (list.length < _limit) {
-        _hasAllAccepted = true;
+        _hasAll = true;
       }
-      log("accepted : ${list.length}");
-      _acceptedAppOffset = list.length;
-      acceptedApplications.addAll(list);
-      acceptedApplications = acceptedApplications.toSet().toList();
-      await setLocalApplications(acceptedAppIsar, list);
+      offset = list.length;
+      applications.addAll(list);
+      applications = applications.toSet().toList();
+      setLocalApplications();
     };
     await performRequest(request: request);
+    var localList = await fetchLocalList();
+    applications.addAll(localList);
+    applications = applications.toSet().toList();
     _isAcceptedLoading = false;
     notifyListeners();
   }
@@ -373,56 +356,16 @@ class BaseUniversityApplicationScreenVM extends ChangeNotifier {
     _isGettingMoreAccepted = true;
     notifyListeners();
     final request = () async {
-      var list = await universityApplicationRepository.getAcceptedApplications(
-          limit: _limit, offSet: _acceptedAppOffset);
+      var list = await fetchNextItems(limit: _limit, offset: offset);
       if (list.length < _limit) {
-        _hasAllAccepted = true;
+        _hasAll = true;
       }
-      _acceptedAppOffset += list.length;
-      acceptedApplications.addAll(list);
-      acceptedApplications = acceptedApplications.toSet().toList();
+      offset += list.length;
+      applications.addAll(list);
+      applications = applications.toSet().toList();
     };
     await performRequest(request: request);
     _isGettingMoreAccepted = false;
-    notifyListeners();
-  }
-
-  Future<void> getPreviousApplications() async {
-    // var list = await getLocalApplications(previousAppIsar);
-    // previousApplications.addAll(list);
-    final request = () async {
-      log("pre previous");
-      var list = await universityApplicationRepository.getPreviousApplications(
-          limit: _limit);
-      if (list.length < _limit) {
-        _hasAllPrevious = true;
-      }
-      log("previous : ${list.length}");
-      _previousAppOffset = list.length;
-      previousApplications.addAll(list);
-      previousApplications = previousApplications.toSet().toList();
-      await setLocalApplications(previousAppIsar, list);
-    };
-    await performRequest(request: request);
-    _isPreviousLoading = false;
-    notifyListeners();
-  }
-
-  Future<void> getNextPreviousApplications() async {
-    _isGettingMorePrevious = true;
-    notifyListeners();
-    final request = () async {
-      var list = await universityApplicationRepository.getPreviousApplications(
-          limit: _limit, offSet: _previousAppOffset);
-      if (list.length < _limit) {
-        _hasAllPrevious = true;
-      }
-      _previousAppOffset += list.length;
-      previousApplications.addAll(list);
-      previousApplications = previousApplications.toSet().toList();
-    };
-    await performRequest(request: request);
-    _isGettingMorePrevious = false;
     notifyListeners();
   }
 
@@ -440,10 +383,8 @@ class BaseUniversityApplicationScreenVM extends ChangeNotifier {
   }
 
   Future<void> refresh() async {
-    _acceptedAppOffset = 0;
-    _previousAppOffset = 0;
+    offset = 0;
     await getAcceptedApplications();
-    await getPreviousApplications();
     return;
   }
 
@@ -452,131 +393,38 @@ class BaseUniversityApplicationScreenVM extends ChangeNotifier {
   bool get hasError => _hasError;
 
   void onError() {
-    if (previousApplications.isEmpty && acceptedApplications.isEmpty) {
+    if (applications.isEmpty) {
       _hasError = true;
     }
     _isAcceptedLoading = false;
-    _isPreviousLoading = false;
     _isGettingMoreAccepted = false;
-    _isGettingMorePrevious = false;
     notifyListeners();
   }
 
-  bool get isGettingMorePrevious => _isGettingMorePrevious;
+  final isarService = IsarService<UniversityApplicationModel>();
 
-  String _selectedStatus = AppStrings.applied;
-
-  Isar? acceptedAppIsar;
-  Isar? previousAppIsar;
-  bool isIsar = false;
-
-  Future<void> setIsarInstances() async {
+  Future<void> setLocalApplications() async {
     try {
-      final dir = await getApplicationDocumentsDirectory();
-      acceptedAppIsar = await Isar.open([UniversityApplicationModelSchema],
-          directory: dir.path, name: "accepted_application");
-      previousAppIsar = await Isar.open([UniversityApplicationModelSchema],
-          directory: dir.path, name: "previous_application");
-      isIsar = true;
+      isarService.saveAll(applications);
     } catch (e) {
-      log("error setting isar error: ${e.toString()}");
-    }
-  }
-
-  Future<List<UniversityApplicationModel>> getLocalApplications(
-      Isar? isar) async {
-    if (!isIsar) {
-      return [];
-    }
-    try {
-      var collection = await isar!.collection<UniversityApplicationModel>();
-      var list = await collection.where().findAll();
-      log("local list ${list.length}");
-      return list;
-    } catch (e) {
-      log("isar error : ${e.toString()}");
       // TODO
     }
-    return [];
-  }
-
-  Future<void> setLocalApplications(
-      Isar? isar, List<UniversityApplicationModel> list) async {
-    if (!isIsar) {
-      return;
-    }
-    // final _limit = 10;
-    // if (list.length > _limit) {
-    //   list = List<UniversityApplicationModel>.generate(
-    //       _limit, (index) => list[index]);
-    // }
-    try {
-      var collection = await isar!.collection<UniversityApplicationModel>();
-      await isar.writeTxn(
-        () => collection.where().deleteAll(),
-      );
-      await isar.writeTxn(
-        () => collection.putAll(list),
-      );
-    } catch (e) {
-      log("isar saving error : ${e.toString()}");
-      // TODO
-    }
-  }
-
-  @override
-  void dispose() {
-    acceptedAppIsar?.close();
-    previousAppIsar?.close();
-    super.dispose();
   }
 
   void addUniversityApp(model) {
-    if (model != null && model is UniversityApplicationModel) {
-      if (previousApplications.isEmpty) {
-        previousApplications.add(model);
-      } else {
-        previousApplications.insert(0, model);
-      }
-      notifyListeners();
-      updateLocalInstance(model, acceptedAppIsar!);
+    applications.insert(0, model);
+  }
+
+  void updateUniversityModel(model) {
+    int index = applications.indexOf(model);
+    if (index > -1) {
+      applications[index] = model;
     }
   }
 
-  void updateUniversityModel(model, {bool isPrevious = true}) {
-    if (model != null && model is UniversityApplicationModel) {
-      int index = 0;
-      if (isPrevious) {
-        index = previousApplications.indexOf(model);
-        if (index > -1) {
-          previousApplications.add(model);
-        }
-      } else {
-        index = acceptedApplications.indexOf(model);
-        if (index > -1) {
-          acceptedApplications.add(model);
-        }
-      }
-      notifyListeners();
-      updateLocalInstance(model, previousAppIsar!);
-    }
-  }
-
-  Future<void> updateLocalInstance(
-      UniversityApplicationModel model, Isar isar) async {
+  Future<void> updateLocalInstance(UniversityApplicationModel model) async {
     try {
-      var collection = isar.collection<UniversityApplicationModel>();
-      var obj = await collection.get(model.id ?? -1);
-      if (obj != null) {
-        await isar.writeTxn(
-          () => collection.put(model),
-        );
-      }
-    } catch (e) {
-      log("Data not locally error: $e");
-    }
-    return;
+      isarService.put(model);
+    } catch (e) {}
   }
-
-//new
 }
