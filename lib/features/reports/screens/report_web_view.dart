fb2386865bfda31cfa27088307f59c1eeb3ea430
Farhan Azam <Farhan.Azam@deutics.com>
2023-10-25 18:40:21 +0500

    webview using webview lib

--- lib/features/reports/screens/report_web_view.dart
+++ lib/features/reports/screens/report_web_view.dart
@@ -1,11 +1,12 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hive_mobile/app/resources/app_theme.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 
 class ReportWebView extends StatefulWidget {
   final String token;
@@ -24,42 +25,42 @@ class _ReportWebViewState extends State<ReportWebView> {
   @override
   Widget build(BuildContext context) {
     final styles = Theme.of(context).extension<AppTheme>()!;
-    final controller = WebViewController()
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setBackgroundColor(const Color(0x00000000))
-      ..setNavigationDelegate(
-        NavigationDelegate(
-          onProgress: (int progress) {
-            // Update loading bar.
-          },
-          onPageStarted: (String url) {},
-          onPageFinished: (String url) {},
-          onWebResourceError: (WebResourceError error) {},
-          onNavigationRequest: (NavigationRequest request) {
-            if (!downloaded) {
-              launchUrl(Uri.parse(request.url));
-              downloaded = true;
-            }
-            if (request.url.endsWith("pdf")) {
-              // launchUrl(Uri.parse(request.url));
-              NavigationDecision.prevent;
-            }
-            return NavigationDecision.prevent;
-          },
-        ),
-      )
-      ..loadRequest(
-        Uri.parse("https://hive.bcp.net.pk/view-reports-pdf").replace(
-          queryParameters: {
-            "token":
-                // "$token"
-                "gAAAAABlOLgTYVoODpWzuIPaqitum77Z450jHK1JwzZy9ODMFXsmXdOPZ_PWIX6U4TzgZEku_JD70YScY6FyzU3C3Nyd2oSIdQ=="
-          },
-        ),
-
-        // Uri.parse("https://www.google.com"),
-      );
+    // final controller = WebViewController()
+    //   ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    //   ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    //   ..setBackgroundColor(const Color(0x00000000))
+    //   ..setNavigationDelegate(
+    //     NavigationDelegate(
+    //       onProgress: (int progress) {
+    //         // Update loading bar.
+    //       },
+    //       onPageStarted: (String url) {},
+    //       onPageFinished: (String url) {},
+    //       onWebResourceError: (WebResourceError error) {},
+    //       onNavigationRequest: (NavigationRequest request) {
+    //         if (!downloaded) {
+    //           launchUrl(Uri.parse(request.url));
+    //           downloaded = true;
+    //         }
+    //         if (request.url.endsWith("pdf")) {
+    //           // launchUrl(Uri.parse(request.url));
+    //           NavigationDecision.prevent;
+    //         }
+    //         return NavigationDecision.prevent;
+    //       },
+    //     ),
+    //   )
+    //   ..loadRequest(
+    //     Uri.parse("https://hive.bcp.net.pk/view-reports-pdf").replace(
+    //       queryParameters: {
+    //         "token":
+    //             // "$token"
+    //             "gAAAAABlOLgTYVoODpWzuIPaqitum77Z450jHK1JwzZy9ODMFXsmXdOPZ_PWIX6U4TzgZEku_JD70YScY6FyzU3C3Nyd2oSIdQ=="
+    //       },
+    //     ),
+    //
+    //     // Uri.parse("https://www.google.com"),
+    //   );
 
     return Scaffold(
       body: SafeArea(
@@ -97,17 +98,26 @@ class _ReportWebViewState extends State<ReportWebView> {
                     },
                   ),
                 ),
+                initialOptions: InAppWebViewGroupOptions(
+                  crossPlatform: InAppWebViewOptions(useOnDownloadStart: true),
+                ),
                 onWebViewCreated: (controller) {
                   webViewController = controller;
                 },
                 onDownloadStartRequest:
                     (controller, downloadStartRequest) async {
-                  log("message : ");
-                  launchUrl(
-                    Uri.parse(
-                      downloadStartRequest.url.toString(),
-                    ),
+                  final taskId = await FlutterDownloader.enqueue(
+                    url: downloadStartRequest.url.toString(),
+                    savedDir: (await getExternalStorageDirectory())!.path,
+                    showNotification: true,
+                    // show download progress in status bar (for Android)
+                    openFileFromNotification:
+                        true, // click on notification to open downloaded file (for Android)
                   );
+
+                  // launchUrl(
+                  //  downloadStartRequest.url,
+                  // );
                 },
               ),
             )
