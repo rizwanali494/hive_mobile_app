bb89ce37827e41b403a54b4c74e6f5db6db90b64
Farhan Azam <Farhan19300@gmail.com>
2023-11-10 15:49:17 +0500

    Added a delay when fetching new notifications when socket event occurs

--- lib/features/notification/view_models/notification_screen_vm.dart
+++ lib/features/notification/view_models/notification_screen_vm.dart
@@ -53,7 +53,7 @@ class NotificationScreenVM extends BaseApiVM<NotificationModel> {
   void setNotificationListener() {
     notificationListener = webSocketService.dataStream?.listen((event) {
       log("Notification Listener ::::: ${event}");
-      Future.delayed(Duration(seconds: 1)).then((value) => refreshList());
+      Future.delayed(Duration(seconds: 2)).then((value) => refreshList());
     });
   }
 
