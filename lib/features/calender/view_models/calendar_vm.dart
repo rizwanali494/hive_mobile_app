a004da339cbce3b7bb3617513661712f2a692e87
Farhan Azam <Farhan.Azam@deutics.com>
2024-02-23 17:08:36 +0500

    Real time on calendar

--- lib/features/calender/view_models/calendar_vm.dart
+++ lib/features/calender/view_models/calendar_vm.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
@@ -5,6 +6,7 @@ import 'package:hive_mobile/app/exceptions/base_exception_controller.dart';
 import 'package:hive_mobile/app/models/data/activity_model.dart';
 import 'package:hive_mobile/app/services/api_services/api_services.dart';
 import 'package:hive_mobile/app/services/local_services/isar_service.dart';
+import 'package:hive_mobile/app/services/web_socket_services/api_event_handler.dart';
 import 'package:hive_mobile/features/calender/controllers/clean_calendar_controller.dart';
 import 'package:hive_mobile/features/calender/repositories/calendar_repo.dart';
 import 'package:intl/intl.dart';
@@ -22,7 +24,7 @@ class CalendarVM extends ChangeNotifier with BaseExceptionController {
   List<int> years = [
     ...List.generate(
       _totalYears + 5 + 1,
-      (index) => ((_startYear - _totalYears) + index),
+          (index) => ((_startYear - _totalYears) + index),
     ),
   ];
   List<String> shortWeekDays = [
@@ -50,22 +52,22 @@ class CalendarVM extends ChangeNotifier with BaseExceptionController {
   CalendarVM() {
     calendarRepo = CalendarRepoImpl(apiService: apiService);
     getAllEvents();
+    setApiEventHandler();
   }
 
   List<ActivityModel> activities = [];
   IsarService<ActivityModel> localService = IsarService();
   late CalendarRepo calendarRepo;
 
-  Future<void> getAllEvents() async {
-    isLoading = true;
+  Future<void> getAllEvents({bool showLoading = true}) async {
+    isLoading = showLoading;
     notifyListeners();
-    activities = [];
     final request = () async {
       var list = await calendarRepo.getAllEvents(
         startDate: DateTime(selectedValue),
         endDate: DateTime(selectedValue, 12, 31),
       );
-      activities.addAll(list);
+      activities = list;
     };
     await performRequest(
       request: request,
@@ -77,8 +79,7 @@ class CalendarVM extends ChangeNotifier with BaseExceptionController {
     notifyListeners();
   }
 
-  Future<void> performRequest(
-      {required Function request, Function(dynamic error)? onError}) async {
+  Future<void> performRequest({required Function request, Function(dynamic error)? onError}) async {
     try {
       await request();
     } catch (error) {
@@ -94,7 +95,7 @@ class CalendarVM extends ChangeNotifier with BaseExceptionController {
       var activityDate =
           DateTime.tryParse(element.date ?? "") ?? DateTime.now();
       var formattedDate =
-          DateTime(activityDate.year, activityDate.month, activityDate.day);
+      DateTime(activityDate.year, activityDate.month, activityDate.day);
       // log("left date = ${formattedDate}");
       // log("right date = ${dt}");
       return formattedDate == dt;
@@ -108,5 +109,25 @@ class CalendarVM extends ChangeNotifier with BaseExceptionController {
   Future<void> refreshCalendar() async {
     await getAllEvents();
     return;
+  }
+
+  void setApiEventHandler() {
+    apiEventHandler = APIEventHandler(
+      handleEvent: handleApiEvent,
+      apiEventTypes: ["ACTIVITY"],
+    );
+  }
+
+  APIEventHandler? apiEventHandler;
+
+  void handleApiEvent(data) {
+    getAllEvents(showLoading: false);
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    apiEventHandler?.dispose();
+    super.dispose();
   }
 }
